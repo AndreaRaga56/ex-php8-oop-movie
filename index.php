6acd53267d125a4d2db2c2183205f87899ec342b
AndreaRaga56 <andrearaga56@gmail.com>
2025-04-04 00:05:46 +0200

    feat genre

--- index.php
+++ index.php
@@ -1,23 +1,44 @@
 <?php
+
+class Genre{
+    public $genere;
+    public $vietatoAiMinori;
+
+    public function __construct($_genere, $_vietatoAiMinori)
+    {
+        $this->genere = $_genere;
+        $this->vietatoAiMinori = $_vietatoAiMinori;
+    }
+}
+
 class Movie
 {
     public $titolo;
     public $regista;
+    public $genere;
     protected $anno;
 
     public function getAnno()
     {
-        return "L'anno di pubblicazione è il" . $this->anno;
+        return "L'anno di pubblicazione è il " . $this->anno;
     }
 
     public function getInfo(){
-        return "Il titolo del film è ". $this->titolo .", è stato diretto da " . $this->regista ." e l'anno di pubblicazione è il" . $this->anno;
+        return "Il titolo del film è " . $this->titolo . ", è stato diretto da " . $this->regista . " e l'anno di pubblicazione è il " . $this->anno;
     }
 
-    public function __construct($_titolo, $_regista, $_anno)
+    public function __construct($_titolo, $_regista, $_anno, Genre $genere)
     {
         $this->titolo = $_titolo;
         $this->regista = $_regista;
         $this->anno = $_anno;
     }
 }
+
+
+$film1 = new Movie("2001: Odissea nello spazio", "Stanley Kubrick", 1968, new Genre("Fantascienza", false));
+$film2 = new Movie("Ex Machina", "Alex Garland", 2015, new Genre("Thriller", false));
+
+echo $film1->getInfo();
+echo "<br>";
+echo $film2->getInfo();
